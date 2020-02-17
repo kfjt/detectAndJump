46d63952322d2d60f090b20d2b32b6d25bcf41ba
kfjt <k.fujita@jcom.home.ne.jp>
2020-02-17 14:47:55 +0900

    info

--- classify.js
+++ classify.js
@@ -1,11 +1,13 @@
 'use strict';
 {
 const video = document.querySelector('video');
+const infoMsg = document.querySelector('#infoMsg');
 const classifyElement = document.querySelector('#classifyMsg');
 
 
 video.onplaying = (event) => {
     console.log('Video is no longer paused.');
+    infoMsg.innerHTML = 'now loading'
     // Load the model.
     mobilenet.load().then(model => {
         // Classify the image.
@@ -14,6 +16,7 @@ video.onplaying = (event) => {
             console.log(predictions);
             const queryword = predictions[0].className;
             video.pause();
+            infoMsg.innerHTML = ''
             classifyElement.innerHTML = queryword
             // location.href = `https://www.google.com/search?q=${queryword}`;
         });
