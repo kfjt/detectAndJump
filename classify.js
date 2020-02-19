b1b8f769fcb142198be20a90193dea0fa4e96879
kfjt <k.fujita@jcom.home.ne.jp>
2020-02-19 15:40:07 +0900

    comment out canvas

--- classify.js
+++ classify.js
@@ -1,13 +1,13 @@
 'use strict';
 {
 const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
+// const canvas = document.querySelector('canvas');
 
 video.onplaying = (event) => {
     Msg.info = 'loading classify model'
     mobilenet.load().then(model => {
         Msg.info = 'loaded classify model'
-        model.classify(canvas).then(predictions => {
+        model.classify(video).then(predictions => {
             Msg.info = `Predictions: ${predictions}`;
             const queryword = predictions[0].className;
             video.pause();
--- video.js
+++ video.js
@@ -2,7 +2,7 @@
 
 {
 const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
+// const canvas = document.querySelector('canvas');
 
 Msg.info = 'Initializing'
 
@@ -17,20 +17,20 @@ navigator.mediaDevices.getUserMedia({video: { facingMode: "environment" }})
 })
 .catch(error => Msg.error = `getUserMedia error: ${error}`);
 
-const drawFrame = () => {
-  const ctx = canvas.getContext('2d')
-  ctx.drawImage(video, 0, 0)
-  requestAnimationFrame(drawFrame)
-}
+// const drawFrame = () => {
+//   const ctx = canvas.getContext('2d')
+//   ctx.drawImage(video, 0, 0)
+//   requestAnimationFrame(drawFrame)
+// }
 
-video.addEventListener('loadedmetadata', () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  video.style.display = 'none';
-});
+// video.addEventListener('loadedmetadata', () => {
+//   canvas.width = video.videoWidth;
+//   canvas.height = video.videoHeight;
+//   video.style.display = 'none';
+// });
 video.addEventListener('loadedmetadata', () => {
   Msg.info = 'video play'
   video.play();
 });
-video.addEventListener('loadeddata', drawFrame)
+// video.addEventListener('loadeddata', drawFrame)
 }
