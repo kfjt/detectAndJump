d0c70be3693619619e75d141201b023037b4a7ff
kfjt <k.fujita@jcom.home.ne.jp>
2020-02-19 11:33:10 +0900

    msg

--- video.js
+++ video.js
@@ -7,10 +7,7 @@ Msg.info = 'Initializing'
 
 const constraints = window.constraints = {
   audio: false,
-  video: {
-    facingMode: "environment",
-    frameRate: { ideal: 10, max: 15 }
-  }
+  video: { facingMode: "environment" }
 };
 
 if (!confirm('This site uses a camera')) {
@@ -21,8 +18,7 @@ navigator.mediaDevices.getUserMedia(constraints)
 .then(function(stream) {
   Msg.info = 'stream start'
   const videoTracks = stream.getVideoTracks();
-  Msg.info = 'Got stream with constraints:', constraints;
-  Msg.info = 'Using video device: ' + videoTracks[0].label;
+  Msg.info = `Using video device: ${videoTracks[0].label}`;
   stream.onremovetrack = function() {
     Msg.info = 'Stream ended';
   };
@@ -35,14 +31,13 @@ navigator.mediaDevices.getUserMedia(constraints)
 })
 .catch(function(error) {
   if (error.name === 'ConstraintNotSatisfiedError') {
-    Msg.error('The resolution ' + constraints.video.width.exact + 'x' +
-        constraints.video.width.exact + ' px is not supported by your device.');
+    Msg.error = 'The resolution ' + constraints.video.width.exact + 'x' +
+        constraints.video.width.exact + ' px is not supported by your device.';
   } else if (error.name === 'PermissionDeniedError') {
-    Msg.error('Permissions have not been granted to use your camera and ' +
+    Msg.error = 'Permissions have not been granted to use your camera and ' +
       'microphone, you need to allow the page access to your devices in ' +
-      'order for the demo to work.');
+      'order for the demo to work.';
   }
-  Msg.error= 'getUserMedia error: ' + error.name, error;
+  Msg.error = `getUserMedia error: ${error.name}, ${error}`;
 });
-
 }
