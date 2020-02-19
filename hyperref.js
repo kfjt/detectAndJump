c98f78bd1fc3aed65630a78a7832e242ca29d1cb
kfjt <k.fujita@jcom.home.ne.jp>
2020-02-19 15:00:03 +0900

    rm comment

--- hyperref.js
+++ hyperref.js
@@ -2,25 +2,16 @@
 {
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-        // if(mutation.type === 'childList') {
-            if(0 < mutation.addedNodes.length) {
-                const url = `https://www.google.com/search?q=${Msg.classify}`
-                if (!confirm(`jump to ${url}`)) {
-                    throw new Error('Access denied by user action');
-                }
-                location.href = url;
-            };
-        // };
+        if(0 < mutation.addedNodes.length) {
+            const url = `https://www.google.com/search?q=${Msg.classify}`
+            if (!confirm(`jump to ${url}`)) {
+                throw new Error('Access denied by user action');
+            }
+            location.href = url;
+        };
     });
 });
 
-const config = {
-    // attributes: true,
-    childList: true,
-    // characterData: true
-};
-
-observer.observe(document.querySelector('#classifyMsg'), config);
-
+observer.observe(document.querySelector('#classifyMsg'), {childList: true});
 // observer.disconnect();
 }
