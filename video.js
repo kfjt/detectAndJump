99777f06261c09f9e57e45f17b356bf72ec0e32f
kfjt <k.fujita@jcom.home.ne.jp>
2020-02-19 13:40:44 +0900

    canvas

--- video.js
+++ video.js
@@ -2,6 +2,7 @@
 
 {
 const video = document.querySelector('video');
+const canvas = document.querySelector('canvas');
 
 Msg.info = 'Initializing'
 
@@ -15,4 +16,18 @@ navigator.mediaDevices.getUserMedia({video: { facingMode: "environment" }})
   video.srcObject = stream
 })
 .catch(error => Msg.error = `getUserMedia error: ${error}`);
+
+const drawFrame = () => {
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(video, 0, 0)
+  requestAnimationFrame(drawFrame)
+}
+
+video.addEventListener('loadedmetadata', () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  video.style.display = 'none';
+});
+video.addEventListener('loadedmetadata', video.play);
+video.addEventListener('loadeddata', drawFrame)
 }
