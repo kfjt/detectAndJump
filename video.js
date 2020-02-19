90c3ed8c76d88c29d3fbb61e16e78fdf0d546f27
kfjt <k.fujita@jcom.home.ne.jp>
2020-02-20 06:29:33 +0900

    video attribute

--- video.js
+++ video.js
@@ -2,6 +2,8 @@
 
 {
 const video = document.querySelector('video');
+video.setAttribute('playsinline', '');
+video.autoplay = true;
 
 Msg.info = 'Initializing'
 
@@ -12,10 +14,7 @@ if (!confirm('This site uses a camera')) {
 navigator.mediaDevices.getUserMedia({video: { facingMode: "environment" }})
 .then(stream => {
   Msg.info = 'stream start';
-  video.setAttribute('playsinline', '');
   video.srcObject = stream;
 })
 .catch(error => Msg.error = `getUserMedia error: ${error}`);
-
-video.addEventListener('loadedmetadata', video.play);
 }
